2bd9160372fb8c7f795befdcb98432c79dc995d6
kristianklimek <klimekkristian@gmail.com>
2018-03-22 14:13:20 +0100

    výpočet obsahu a obvod kruhu funguje tak jak má

--- index.php
+++ index.php
@@ -2,23 +2,22 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>xd</title>
+    <title>Zdarec Koky</title>
   </head>
   <body>
     <h1>Proměné</h1>
 <?php
     define("PI", 3.14) ;
-    $r=5 ;
+    $r=874 ;
     $S= PI*$r*$r;
     $obvod=  2*PI*$r;
 
-
  ?>
- <p> Poloměr : <?php echo $r  ?> </p>
+ <p> Poloměr : <?php echo $r ?> </p>
  <p> Obsah kruhu : <?php echo "S = PI * r * r =  " . PI . " * $r * $r ="  ?> <?php echo $S ?> </p>
  <p> Obvod kruhu : O = <?php echo "2 * PI * r =  2 * " . PI . " * $r " ?> = <?php echo $obvod ?></p>
 
- <div style="width:31.4px ; height:31.4px ; border-radius:20px ;
+ <div style="width:<?php echo $r ?>px ; height:<?php echo $r ?>px ; border-radius:800px ;
              display:block; background:#F00">
  </div>
 
